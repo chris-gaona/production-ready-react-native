faf627eaaa71ff9f1a4dd793a3c24d3e19e5da41
Chris Gaona <thebeloved88@sbcglobal.net>
2018-07-07 20:29:58 -0700

    Saves data for offline use with redux persist

--- App/config/store.js
+++ App/config/store.js
@@ -1,6 +1,8 @@
 import { createStore, applyMiddleware } from 'redux'
 import logger from 'redux-logger'
 import createSagaMiddleware from 'redux-saga'
+import { persistStore, persistCombineReducers } from 'redux-persist'
+import storage from 'redux-persist/lib/storage'
 
 import reducer from '../reducers'
 import rootSaga from './sagas'
@@ -11,8 +13,22 @@ if (process.env.NODE_ENV === 'development') {
   middleware.push(logger)
 }
 
-const store = createStore(reducer, applyMiddleware(...middleware))
+// set up persist
+const persistConfig = {
+  key: 'root',
+  storage,
+  blacklist: ['network'], // specify state items to not persist
+}
 
-sagaMiddleware.run(rootSaga)
+// adds persist to combineReducers (don't use combineReducers with persistCombineReducers)
+const persistReducer = persistCombineReducers(persistConfig, reducer)
 
-export default store
+const configureStore = () => {
+  const store = createStore(persistReducer, applyMiddleware(...middleware))
+  sagaMiddleware.run(rootSaga)
+  const persistor = persistStore(store)
+
+  return { store, persistor }
+}
+
+export default configureStore
--- App/index.js
+++ App/index.js
@@ -1,14 +1,32 @@
-import React from 'react'
+import React, { Component } from 'react'
 import { Provider } from 'react-redux'
-
+import { PersistGate } from 'redux-persist/es/integration/react'
 import Navigator from './config/routes'
 import { AlertProvider } from './components/Alert'
-import store from './config/store'
+import configureStore from './config/store'
 
-export default () => (
-  <Provider store={store}>
-    <AlertProvider>
-      <Navigator onNavigationStateChange={null} />
-    </AlertProvider>
-  </Provider>
-)
+export default class extends Component {
+  constructor(props) {
+    super(props)
+
+    const { store, persistor } = configureStore()
+    this.state = {
+      store,
+      persistor,
+    }
+  }
+
+  render() {
+    return (
+      <Provider store={this.state.store}>
+        <PersistGate persistor={this.state.persistor}>
+          {' '}
+          {/* causes the app to wait for data to be rehydrated in store before rendering */}
+          <AlertProvider>
+            <Navigator onNavigationStateChange={null} />
+          </AlertProvider>
+        </PersistGate>
+      </Provider>
+    )
+  }
+}
