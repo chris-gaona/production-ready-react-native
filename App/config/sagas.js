27800f6356befee2d8efe7fc500d315064252ede
Chris Gaona <thebeloved88@sbcglobal.net>
2018-07-08 20:27:48 -0700

    Handles offline requests with clear error message; clears error message

--- App/config/sagas.js
+++ App/config/sagas.js
@@ -1,4 +1,5 @@
 import { takeEvery, call, put, select } from 'redux-saga/effects'
+import { delay } from 'redux-saga'
 
 import {
   CHANGE_BASE_CURRENCY,
@@ -12,6 +13,17 @@ export const getLatestRate = currency =>
   fetch(`https://fixer.handlebarlabs.com/latest?base=${currency}`)
 
 const fetchLatestConversionRates = function* ({ currency }) {
+  // yield put({ type: CONVERSION_ERROR, error: null })
+  const { connected, hasCheckedStatus } = yield select(state => state.network)
+
+  if (!connected && hasCheckedStatus) {
+    yield put({
+      type: CONVERSION_ERROR,
+      error: 'Not connected to the internet - conversion rate may be outdated or unavailable',
+    })
+    return
+  }
+
   try {
     let usedCurrency = currency
     if (usedCurrency === undefined) {
@@ -29,10 +41,21 @@ const fetchLatestConversionRates = function* ({ currency }) {
   }
 }
 
+const clearConversionError = function* () {
+  const DELAY_SECONDS = 4 // approx. time warning is shown
+  const error = yield select(state => state.currencies.error)
+  if (error) {
+    // check for existance otherwise we get stuck in an infinite loop
+    yield delay(DELAY_SECONDS * 1000)
+    yield put({ type: CONVERSION_ERROR, error: null })
+  }
+}
+
 const rootSaga = function* () {
   yield takeEvery(GET_INITIAL_CONVERSION, fetchLatestConversionRates)
   yield takeEvery(CHANGE_BASE_CURRENCY, fetchLatestConversionRates)
   yield takeEvery(SWAP_CURRENCY, fetchLatestConversionRates)
+  yield takeEvery(CONVERSION_ERROR, clearConversionError)
 }
 
 export default rootSaga
