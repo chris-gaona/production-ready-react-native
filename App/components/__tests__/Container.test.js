6be457a87004f0e4c7fee83d1ca91c24b21b7a2e
Chris Gaona <thebeloved88@sbcglobal.net>
2018-07-16 08:34:17 -0700

    Uses component snapshot testing in Jest

--- App/components/__tests__/Container.test.js
+++ App/components/__tests__/Container.test.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { View } from 'react-native'
 import renderer from 'react-test-renderer'
 
 import { Container, styles } from '../Container'
@@ -9,6 +10,21 @@ it('should export a styles object', () => {
 
 it('should render without children', () => {
   const rendered = renderer.create(<Container />).toJSON()
-  console.log(rendered)
   expect(rendered).toBeTruthy()
+})
+
+it('should render children props', () => {
+  const rendered = renderer
+    .create(<Container>
+      <View />
+            </Container>)
+    .toJSON()
+  // toMatchSnapshot creates a snapshot
+  expect(rendered).toMatchSnapshot()
+})
+
+it('should use the specified backgroundColor if provided', () => {
+  const rendered = renderer.create(<Container backgroundColor="red" />).toJSON()
+  // toMatchSnapshot creates a snapshot
+  expect(rendered).toMatchSnapshot()
 })
