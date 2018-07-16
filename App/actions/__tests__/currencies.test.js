e0ea3a48f240231fcb52f4fc2d8ec53e12933e16
Chris Gaona <thebeloved88@sbcglobal.net>
2018-07-16 08:48:36 -0700

    Tests redux actions with jest snapshots instead

--- App/actions/__tests__/currencies.test.js
+++ App/actions/__tests__/currencies.test.js
@@ -2,22 +2,29 @@ import { getInitialConversion, changeCurrencyAmount } from '../currencies'
 
 describe('getInitialConversion', () => {
   it('should create a properly formatted action', () => {
-    const expected = { type: 'GET_INITIAL_CONVERSION' }
-    const actual = getInitialConversion()
-    expect(actual).toEqual(expected)
+    // const expected = { type: 'GET_INITIAL_CONVERSION' }
+    // const actual = getInitialConversion()
+    // expect(actual).toEqual(expected)
+
+    // press u to rewrite snapshot in terminal where jest is running
+    expect(getInitialConversion()).toMatchSnapshot()
   })
 })
 
 describe('changeCurrencyAmount', () => {
   it('should create an action with an amount of 100', () => {
-    const expected = { type: 'CHANGE_CURRENCY_AMOUNT', amount: 100 }
-    const actual = changeCurrencyAmount(100)
-    expect(actual).toEqual(expected)
+    // const expected = { type: 'CHANGE_CURRENCY_AMOUNT', amount: 100 }
+    // const actual = changeCurrencyAmount(100)
+    // expect(actual).toEqual(expected)
+
+    expect(changeCurrencyAmount(100)).toMatchSnapshot()
   })
 
   it('should convert a string value to a float', () => {
-    const expected = { type: 'CHANGE_CURRENCY_AMOUNT', amount: 100 }
-    const actual = changeCurrencyAmount('100')
-    expect(actual).toEqual(expected)
+    // const expected = { type: 'CHANGE_CURRENCY_AMOUNT', amount: 100 }
+    // const actual = changeCurrencyAmount('100')
+    // expect(actual).toEqual(expected)
+
+    expect(changeCurrencyAmount('100')).toMatchSnapshot()
   })
 })
