b3278bb8de7887b9ca6432626117b959457bd40c
Chris Gaona <thebeloved88@sbcglobal.net>
2018-07-16 09:02:48 -0700

    Adds tests for redux reducers

---
+++ App/reducers/__tests__/currencies.test.js
@@ -0,0 +1,26 @@
+import reducer from '../currencies'
+import { getInitialConversion } from '../../actions/currencies'
+
+const initialState = {
+  baseCurrency: 'USD',
+  quoteCurrency: 'GBP',
+  amount: 100,
+  conversions: {},
+  error: null,
+}
+
+it('should set initial state', () => {
+  const expected = { ...initialState }
+  const actual = reducer(undefined, {})
+  expect(actual).toEqual(expected)
+})
+
+it('should set the nested data on initial fetch', () => {
+  const expected = {
+    ...initialState,
+    conversions: { USD: { date: '', isFetching: true, rates: {} } },
+  }
+
+  const actual = reducer(undefined, getInitialConversion())
+  expect(actual).toEqual(expected)
+})
